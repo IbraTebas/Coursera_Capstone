58ed9b768bc6be8464efca71791a4483935d6745
Ibrahim V <35322625+IbraTebas@users.noreply.github.com>
2019-10-18 22:01:35 +1000

    add a python script that prints a message to the Project

---
+++ HelloCapstoneProject.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#This notebool is for the Peer-graded Assignment: Capstone Project Notebook
+
+
+# In[2]:
+
+
+import numpy as np # library for vectorized computation
+import pandas as pd # library to process data as dataframes
+
+
+# In[3]:
+
+
+print('Hello Capstone Project Course!')
+
+
+# In[ ]:
+
+
+
+
